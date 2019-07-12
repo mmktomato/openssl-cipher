82ffce5952fd56c14e796f33ef4f6914fffc34da
mmktomato <mkt.motohashi@gmail.com>
2019-07-12 15:46:19 +0900

    use getopts

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,6 @@
 
 set -eu
 
-mode=${1:-'-h'}
-in=${2:-''}
-out=${3:-''}
-
-cipher_opt=-aes-256-cbc
-
 show_usage() {
     echo 'Usage: [-h|-e|-d|-l] [input_file] [output_file]'
     echo
@@ -17,14 +11,50 @@ show_usage() {
     echo '-d) decrypts [input_file] and writes to [output_file].'
 }
 
+mode=
+
+while getopts hled OPT
+do
+    case $OPT in
+        h)
+            show_usage
+            exit
+            ;;
+        l)
+            openssl list-cipher-commands
+            exit
+            ;;
+        e)
+            mode=encrypt
+            ;;
+        d)
+            mode=decrypt
+            ;;
+        \?)
+            show_usage
+            exit
+            ;;
+    esac
+done
+shift $((OPTIND - 1))
+
+in=${1:-''}
+out=${2:-''}
+cipher_opt=-aes-256-cbc
+
 check_args_for_enc_and_dec() {
+    if [ "$in" = "" -o "$out" = "" ]; then
+        echo "input file and output file must be specified."
+        return 1
+    fi
+
     if [ ! -f $in ]; then
-        echo "$in does not exist."
+        echo "'$in' does not exist."
         return 1
     fi
 
     if [ -f $out ]; then
-        echo "$out already exists."
+        echo "'$out' already exists."
         return 1
     fi
 
@@ -32,20 +62,14 @@ check_args_for_enc_and_dec() {
 }
 
 case $mode in
-    -h)
-        show_usage
-        ;;
-    -l)
-        openssl list-cipher-commands
-        ;;
-    -e)
+    encrypt)
         check_args_for_enc_and_dec && openssl enc -e $cipher_opt -in $in -out $out
         ;;
-    -d)
+    decrypt)
         check_args_for_enc_and_dec && openssl enc -d $cipher_opt -in $in -out $out
         ;;
     *)
-        show_usage
+        echo "mode must be specified."
         ;;
 esac
 
